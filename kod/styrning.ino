7f4bed71d7a4eea3b788d217515a27e9bed4283a
bryndav <d.bryngelsson@gmail.com>
2020-09-10 16:28:33 +0200

    Implementerat en robustare tillståndsmaskin.

--- kod/styrning.ino
+++ kod/styrning.ino
@@ -1,23 +1,49 @@
-/* Enkel tidsstyrning av två utgångar */
+/*****************************************************************************
+*																			 *
+*	Simple program to blink two leds in different patterns. The program is	 *
+*	made a bit more complex by implementing a state machine to handle The 	 *	
+*	flashing of leds. 														 *
+*																			 *
+*****************************************************************************/
 
 #define rightPump 2 
 #define leftPump 4
+#define ON 1
+#define OFF 0
 
-#define state_1 5000
-#define state_2 10000
-#define state_3 13000
-#define state_4 16000
+enum state_def {
+	state_1 = 5000,
+	state_2 = 10000,
+	state_3 = 13000,
+	state_4 = 16000,	
+};
 
-bool checkState(int state);
-void changeState(int state);
+struct State {
+	int state_nr;
+	uint8_t leftPumpVal;
+	uint8_t rightPumpVal;
+	int state_start;
+	int state_end;
+	struct State* next;
+};
 
-unsigned long prevMillis = 0;
-int currState = 0; 
+struct State state1 = { state_1,  OFF, OFF, 0, state_1, NULL };
+struct State state2 = { state_2,  ON, ON, state_1, state_2, NULL };
+struct State state3 = { state_3,  ON, OFF, state_2, state_3, NULL };
+struct State state4 = {state_4,  OFF, ON, state_3, state_4, NULL};
+struct State* currentState = NULL;
 
-uint8_t leftPumpVal = 0;
-uint8_t rightPumpVal = 0;
+bool checkState();
+void changeState();
 
 void setup() {
+	// Linking the state machine
+	state1.next = &state2;
+	state2.next = &state3;
+	state3.next = &state4;
+	state4.next = &state1;
+	currentState = &state1;
+
 	pinMode(rightPump, OUTPUT);
 	pinMode(leftPump, OUTPUT);
 
@@ -28,80 +54,55 @@ void setup() {
 
 void loop() {
 	bool stateChange = false;
+
+	stateChange = checkState();
+
+	if(stateChange){
+		changeState();
+	}
+}
+
+
+/*****************************************************************************
+*																			 *
+*	Checks if the time passed since boot has surpassed the max state time.	 *
+*	Returns a boolean with true if max state time has passed i.e we need	 *
+*	to change the state. Else it returns false.								 *																			 *
+*																			 *
+*****************************************************************************/
+
+bool checkState() {
+	static unsigned long prevMillis = 0;
 	unsigned long currMillis = 0;
 	unsigned long diff = 0;
+	bool changeState = false;
 
 	currMillis = millis();
 	diff = currMillis - prevMillis;
 
-	if (diff <= state_1){
-		currState = state_1;
-	}else if (diff > state_1 && diff <= state_2){
-		currState = state_2;
-	}else if (diff > state_2 && diff <= state_3){
-		currState = state_3;
-	}else if (diff > state_3 && diff <= state_4){
-		currState = state_4;
-	}else{
-		prevMillis = currMillis;
+	if (diff > currentState->state_end){
+		changeState = true;
+
+		if(currentState->state_nr == state_4){
+			prevMillis = currMillis;
+		}
 	}
 
-	stateChange = checkState(currState);
-
-	if (stateChange){
-		changeState(currState);
-	}
+	return changeState;
 }
 
-bool checkState(int state){
-	bool returnVal = false;
 
-	if (state == state_1){
-		returnVal = (leftPumpVal == 0 && rightPumpVal == 0) ? false : true;
-	}else if (state == state_2){
-		returnVal = (leftPumpVal == 1 && rightPumpVal == 1) ? false : true;
-	}else if (state == state_3){
-		returnVal = (leftPumpVal == 1 && rightPumpVal == 0) ? false : true;
-	}else if (state == state_4){
-		returnVal = (leftPumpVal == 0 && rightPumpVal == 1) ? false : true;
-	}else{
-		returnVal = false;
-	}
+/*****************************************************************************
+*																			 *
+*	Function that handles the actual change of state. Current state pointer	 *
+*	moves and new values are written to the leds. 							 *																			 *
+*																			 *
+*****************************************************************************/
 
-	return returnVal;
-}
+void changeState() {
 
-void changeState(int state){
-	if (state == state_1){
-		leftPumpVal = 0;
-		rightPumpVal = 0;
+	currentState = currentState->next;
 
-		digitalWrite(leftPump, leftPumpVal);
-		digitalWrite(rightPump, rightPumpVal);
-	}else if (state == state_2){
-		leftPumpVal = 1;
-		rightPumpVal = 1;
-
-		digitalWrite(leftPump, leftPumpVal);
-		digitalWrite(rightPump, rightPumpVal);
-	}else if (state == state_3){
-		leftPumpVal = 1;
-		rightPumpVal = 0;
-
-		digitalWrite(leftPump, leftPumpVal);
-		digitalWrite(rightPump, rightPumpVal);
-	}else if (state == state_4){
-		leftPumpVal = 0;
-		rightPumpVal = 1;
-
-		digitalWrite(leftPump, leftPumpVal);
-		digitalWrite(rightPump, rightPumpVal);
-	}else{
-		leftPumpVal = 0;
-		rightPumpVal = 0;
-
-		digitalWrite(leftPump, leftPumpVal);
-		digitalWrite(rightPump, rightPumpVal);
-	}
-
+	digitalWrite(leftPump, currentState->leftPumpVal);
+	digitalWrite(rightPump, currentState->rightPumpVal);
 }
