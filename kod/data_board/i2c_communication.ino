e94fc0187f7dd222b2ee211039879dffbea35e57
bryndav <d.bryngelsson@gmail.com>
2020-11-14 15:42:46 +0100

    Applied code style, renamed gyro -> data_board, separated functions into module

---
+++ kod/data_board/i2c_communication.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+
+const byte slave_address = 15;
+
+int 
+sendInt (char   identifier,
+         int*   value)
+{
+  int returnVal;
+
+  Wire.beginTransmission (slave_address);
+  Wire.write (identifier);
+  Wire.write (highByte (*value));
+  Wire.write (lowByte (*value));    
+  returnVal = Wire.endTransmission ();
+
+  return returnVal;
+}
+
+int
+sendFloat(char    identifier,
+          float*  value)
+{
+  byte f_byteArray[4];
+  float temp_Depth;
+  int return_val;
+  
+  float2Bytes (*value, &f_byteArray[0]);
+  Wire.beginTransmission (slave_address);
+  Wire.write (identifier);
+  
+  for (int i = 0; i < sizeof (f_byteArray); i++) {
+    Wire.write (f_byteArray[i]);
+  }
+  return_val = Wire.endTransmission ();
+
+  return return_val;
+}
+
+void 
+float2Bytes (float  val, 
+             byte*  bytes_array) 
+{
+  union {
+    float float_variable;
+    byte temp_array[4];
+  } u;
+    
+  u.float_variable = val;
+  memcpy (bytes_array, u.temp_array, 4);
+}
